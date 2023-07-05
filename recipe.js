fd7e11384f3254de5a3028203396d414c2dc5970
Treasure-Mayowa <69307446+Treasure-Mayowa@users.noreply.github.com>
2023-07-05 23:09:08 +0000

    Update recipe.js

--- recipe.js
+++ recipe.js
@@ -1,5 +1,9 @@
+const apikey = API_KEY
+
 function submit() {
     var input = document.getElementById("input");
     var ingred = input.split(", ").trim()
     input.innerHTML = ""
 }
+const ingredients = ""
+const URL = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${apikey}&ingredients=4{ingredients}`
