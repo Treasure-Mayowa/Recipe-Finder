3fc50a57b75d91ce2e4b384c91f2fc74f6f84029
Treasure Mayowa <69307446+Treasure-Mayowa@users.noreply.github.com>
2023-07-21 23:25:03 +0000

    Update recipe.js

--- recipe.js
+++ recipe.js
@@ -1,20 +1,40 @@
 // const website = "https://spoonacular.com/food-api/docs#Search-Recipes-by-Ingredients"
 
 const apikey = '9041fdff59e7495ba9ae0d5607683c20'
-const recipe = document.querySelector("#Recipe")
+const result = document.querySelector("#results")
 const form = document.querySelector("form")
-const search =  document.getElementById("input")
-const ingredients = ""
+const search =  document.getElementById("input").value
 
 
 // Check for form submission
 form.addEventListener(
     "submit",
     function(event) {
-        getRecipe(search.value)
-        event.preventDefault();
+        getRecipe(search)
     }
 )
+
+function getRecipe(value){
+    result.innerHTML = `<h2>Loading...</h2>`
+
+    var valueArray = value.split(",");
+
+    var trimmedArray = valueArray.map((ingredient) => ingredient.trim());
+
+    const value = trimmedArray.join(",");
+
+    
+
+    result.innerHTML = `
+    <img class="card-img-top" src="..." alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    `
+}
+
 
 var acc = document.getElementsByClassName("accordion");
 var i;
